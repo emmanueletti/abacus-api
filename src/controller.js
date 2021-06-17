c428ed26e4fbf37b37318b4e75529194bd606c66
emmanueletti <78582921+emmanueletti@users.noreply.github.com>
2021-06-17 15:18:07 -0700

    finished basic api functionality and added cors *

--- src/controller.js
+++ src/controller.js
@@ -1,23 +1,21 @@
+const http = require('http');
+
 // @desc calculate investment returns with and without fees
 // @route POST /api/investment-returns
-const calculateInvestmentReturns = (req, res) => {
+const calculateInvestmentReturns = (req, res, headers) => {
 	// get x-www-form-urlencoded form data found in body and concated data chunks into variable
+
 	let body = '';
 	req.on('data', (chunk) => {
 		body += chunk.toString();
 	});
 
-	// create object when body data is fully streamed
 	req.on('end', () => {
-		// process form data string into object
-		const formData = body.split('&').map((element) => element.split('='));
-		const formDataObject = Object.fromEntries(formData);
-
 		// CALCULATION
 		// compount interest formula from https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php
 		const investmentFeesResult = {};
 
-		let { currentAmount: p, annualContribution: pmt, retirementAge, currentAge, annualReturn, mer } = formDataObject;
+		let { currentAmount: p, annualContribution: pmt, retirementAge, currentAge, annualReturn, mer } = JSON.parse(body);
 		let t = parseInt(retirementAge, 10) - parseInt(currentAge, 10);
 		// using number because don't know if will get while number or decimal from user input
 		let r = Number(annualReturn) / 100;
@@ -32,7 +30,7 @@ const calculateInvestmentReturns = (req, res) => {
 		investmentFeesResult.totalWithFees = cifpWithFees + fvsWithFees;
 
 		// RESPONSE
-		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.writeHead(200, headers);
 		res.end(JSON.stringify(investmentFeesResult));
 	});
 };
--- src/server.js
+++ src/server.js
@@ -2,10 +2,26 @@ const http = require('http');
 const { calculateInvestmentReturns } = require('./controller.js');
 
 const server = http.createServer((req, res) => {
-	if (req.method === 'POST' && req.url === '/api/investment-returns') {
-		calculateInvestmentReturns(req, res);
+	// https://bigcodenerd.org/enable-cors-node-js-without-express/
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': '*',
+		'Access-Control-Allow-Methods': 'OPTIONS, POST',
+		'Access-Control-Max-Age': 2592000, // 30 days in seconds
+		/** add other headers as per requirement */
+		'Content-Type': 'application/json',
+	};
+
+	if (req.method === 'OPTIONS') {
+		console.log('recieved a pre-flight request');
+		res.writeHead(204, headers);
+		res.end(JSON.stringify({ message: 'recieved pre-flight request' }));
+	} else if (req.method === 'POST' && req.url === '/api/investment-returns') {
+		console.log('recieved a good request');
+		calculateInvestmentReturns(req, res, headers);
 	} else {
-		res.writeHead(404, { 'Content-Type': 'application/json' });
+		console.log('recieved a bad request');
+		res.writeHead(404, headers);
 		res.end(JSON.stringify({ message: 'route not found' }));
 	}
 });
