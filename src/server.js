2066f35e9df1c0b57231d677cc202e9326a86781
emmanueletti <78582921+emmanueletti@users.noreply.github.com>
2021-06-17 18:01:37 -0700

    project is now fully functional, now in need of refactoring and code clean up

--- src/server.js
+++ src/server.js
@@ -13,14 +13,14 @@ const server = http.createServer((req, res) => {
 	};
 
 	if (req.method === 'OPTIONS') {
-		console.log('recieved a pre-flight request');
+		// console.log('recieved a pre-flight request');
 		res.writeHead(204, headers);
 		res.end(JSON.stringify({ message: 'recieved pre-flight request' }));
 	} else if (req.method === 'POST' && req.url === '/api/investment-returns') {
-		console.log('recieved a good request');
+		// console.log('recieved a good request');
 		calculateInvestmentReturns(req, res, headers);
 	} else {
-		console.log('recieved a bad request');
+		// console.log('recieved a bad request');
 		res.writeHead(404, headers);
 		res.end(JSON.stringify({ message: 'route not found' }));
 	}
@@ -29,5 +29,5 @@ const server = http.createServer((req, res) => {
 const PORT = process.env.port || 5000;
 
 server.listen(PORT, () => {
-	console.log(`SERVER LISTENING ON PORT ${PORT}`);
+	// console.log(`SERVER LISTENING ON PORT ${PORT}`);
 });
